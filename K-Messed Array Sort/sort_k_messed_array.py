014a2c3c037136d942dd6dfe5684d80471a4c68c
Visal <visal@Visals-MBP-712.T-mobile.com>
2020-03-19 20:58:57 -0700

    added K-Messed Array Sort solution and test

---
+++ K-Messed Array Sort/sort_k_messed_array.py
@@ -0,0 +1,22 @@
+def sort_k_messed_array(arr, k):
+  output = [0] * len(arr)
+  
+  mySet = set()
+  
+  for i in range(len(arr)):
+    start = i - k 
+    end = i + k + 1
+    current_min = float('inf')
+    current_index = -1 
+    for j in range(start, end):
+      if ((j%len(arr)) not in mySet) and (current_min > arr[j% len(arr)]):
+        current_min = arr[j % len(arr)]
+        current_index = j % len(arr)
+        
+    output[i] = current_min
+    mySet.add(current_index)
+
+    
+  
+  
+  return output
