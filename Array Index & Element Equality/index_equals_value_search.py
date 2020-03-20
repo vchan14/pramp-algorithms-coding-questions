b721e01bb4c3e1d800c2968420709353b416e96d
vchan14 <chanvisalbotr@gmail.com>
2020-03-19 23:50:30 -0700

    added Array Index & Element Equality

---
+++ Array Index & Element Equality/index_equals_value_search.py
@@ -0,0 +1,23 @@
+def index_equals_value_search(arr):
+  size = len(arr)
+  result = -1
+  result = helper(0, size-1, arr)  
+  return result 
+  
+  
+def helper(lower, upper, arr):
+  mid = (upper + lower)// 2
+  val = arr[mid]
+  
+  if(lower > upper):
+    return -1 
+  if(val > mid): 
+    return helper(lower, mid-1, arr)
+  
+  elif(val < mid):
+    return helper(mid+1, upper, arr)
+  
+  else:
+    return val 
+    
+    
