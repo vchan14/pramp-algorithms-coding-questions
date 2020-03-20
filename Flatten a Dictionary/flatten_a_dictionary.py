e1398354efa1139007ae11e5c43d5e8b9731e46d
vchan14 <chanvisalbotr@gmail.com>
2020-03-19 18:59:22 -0700

    Flatten a Dictionary

---
+++ Flatten a Dictionary/flatten_a_dictionary.py
@@ -0,0 +1,31 @@
+
+def newKey(str1, str2):
+    if len(str1) == 0:
+        return str2
+    
+    if len(str2) == 0:
+        return str1
+    
+    return(str1 + "." + str2)
+    
+
+
+
+def helper(initial_key, obj, output):
+    if type(obj) is dict:
+        for key, value in obj.items():
+            helper(newKey(initial_key, key), value, output)
+    else:
+        output[initial_key] = obj
+
+def flatten_dicionary(dictionary):
+    output = {}
+    helper("", dictionary, output)
+
+    return output
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Flatten a Dictionary/test.py
@@ -0,0 +1,49 @@
+import unittest
+from flatten_a_dictionary import flatten_dicionary
+
+
+class flatten_dictonary_Case(unittest.TestCase):
+    def case_1(self):
+        myInput = {"Key1":"1","Key2":{"a":"2","b":"3","c":{"d":"3","e":"1"}}}
+        expected = {"Key1":"1","Key2.a":"2","Key2.b":"3","Key2.c.d":"3","Key2.c.e":"1"}
+        self.assertEqual(flatten_dicionary(myInput), expected)
+
+
+    def case_2(self):
+        myInput = {"Key":{"a":"2","b":"3"}}
+        expected = {"Key.a":"2","Key.b":"3"}
+        self.assertEqual(flatten_dicionary(myInput), expected)
+      
+
+
+
+    def case_3(self):
+        myInput = {"Key1":"1","Key2":{"a":"2","b":"3","c":{"d":"3","e":{"f":"4"}}}}
+        expected =  {"Key1":"1","Key2.a":"2","Key2.b":"3","Key2.c.d":"3","Key2.c.e.f":"4"}
+        self.assertEqual(flatten_dicionary(myInput), expected)
+
+
+
+    def case_4(self):
+        myInput = {"":{"a":"1"},"b":"3"}
+        expected =  {"a":"1","b":"3"}
+        self.assertEqual(flatten_dicionary(myInput), expected)
+
+    def case_5(self):
+        myInput =  {"a":{"b":{"c":{"d":{"e":{"f":{"":"awesome"}}}}}}}
+        expected =  {"a.b.c.d.e.f":"awesome"}
+        self.assertEqual(flatten_dicionary(myInput), expected)
+
+    def case_6(self):
+        myInput = {"a":"1"}
+        expected = {"a":"1"}
+        self.assertEqual(flatten_dicionary(myInput), expected)
+
+
+
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
